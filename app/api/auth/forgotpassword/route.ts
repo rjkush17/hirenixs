69d843c17dc40abe6165bf7b66ad67b8843fbbd9
rjkush17 <rjkush17rishabh@gmail.com>
2025-11-04 22:41:32 +0530

    added forgot-password page and api both

---
+++ app/api/auth/forgotpassword/route.ts
@@ -0,0 +1,62 @@
+import connectDB from "@/database/Database";
+import { NextRequest, NextResponse } from "next/server";
+import { User, IUser } from "@/models/user";
+import crypto from "crypto-js";
+import SendMail from "@/lib/nodemailer/sendMail";
+import type { mailDetailsType } from "@/lib/nodemailer/sendMail";
+
+export const POST = async (request: NextRequest): Promise<NextResponse> => {
+  try {
+    await connectDB();
+    const { identifier } = await request.json();
+
+    const isRegistered: IUser | null = await User.findOne({
+      $or: [{ email: identifier }, { username: identifier }],
+    });
+
+    if (!isRegistered) {
+      return NextResponse.json(
+        { error: "User or Email not found" },
+        { status: 400 },
+      );
+    }
+    const resetToken: string = crypto.lib.WordArray.random(32).toString();
+    console.log("resetToken", resetToken);
+
+    const hashedToken = crypto.SHA256(resetToken).toString();
+
+    isRegistered.resetPasswordToken = hashedToken;
+    isRegistered.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+    await isRegistered.save();
+
+    const encodedEmail = encodeURIComponent(isRegistered.email);
+    const encodedToken = encodeURIComponent(resetToken);
+    const url = `${process.env.BASE_URL}/resetpassword?token=${encodedToken}&mail=${encodedEmail}`;
+
+    const mailBody: mailDetailsType = {
+      from: "Hirenixs Team",
+      to: isRegistered.email,
+      subject: "Forgot Passowrd Link",
+      text: url,
+    };
+    const isSendComplete = await SendMail(mailBody);
+
+    if (!isSendComplete) {
+      return NextResponse.json(
+        { error: "Failed to Send mail" },
+        { status: 400 },
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Reset Link Send to Register Mail" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log("get error in the forgot password route ", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -22,9 +21,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import usePOST from "@/hooks/usePOST";
+import { useState } from "react";
+import { CheckCircle } from "lucide-react";
+import maskEmail from "@/utils/maskEmail";
+import maskUsername from "@/utils/maskUsername";
 
 function Page() {
   const { isLoading, apiCall } = usePOST();
+
+  const [showSuccessScreen, setShowSucessScreen] = useState<boolean>(false);
+  const [identifier, setIdentifer] = useState<string>("");
 
   const form = useForm<forgotPasswordType>({
     resolver: zodResolver(forgotPasswordSchema),
@@ -37,51 +43,78 @@ function Page() {
     toast.promise(apiCall("/api/auth/forgotpassword", data), {
       loading: "Generatings reset Link",
       success: (res: any) => {
+        setShowSucessScreen(true);
+        setIdentifer(data.identifier);
         return res;
       },
       error: (err: any) => err.message || "Fail to Generate forgot Link|",
     });
   };
-
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Forgot Password</CardTitle>
-          <CardDescription>
-            Enter your email or username to receive a reset link.
-          </CardDescription>
-        </CardHeader>
+      {!showSuccessScreen && (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Forgot Password</CardTitle>
+            <CardDescription>
+              Enter your email or username to receive a reset link.
+            </CardDescription>
+          </CardHeader>
 
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="identifier"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username or Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Username or Email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="identifier"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username or Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Username or Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {!isLoading ? (
+                  <Button type="submit" className="w-full">
+                    Send Reset Link
+                  </Button>
+                ) : (
+                  <Button className="w-full" disabled>
+                    Sending Reset Link
+                  </Button>
                 )}
-              />
-              {!isLoading ? (
-                <Button type="submit" className="w-full">
-                  Send Reset Link
-                </Button>
-              ) : (
-                <Button className="w-full" disabled>
-                  Sending Reset Link
-                </Button>
-              )}
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      )}
+      {showSuccessScreen && (
+        <Card className="max-w-md mx-auto mt-10 border-green-500">
+          <CardHeader className="flex flex-col items-center">
+            <CheckCircle className="w-12 h-12 text-green-500 mb-2" />
+            <CardTitle className="text-lg font-semibold text-center">
+              Success!
+            </CardTitle>
+            <CardDescription className="text-center text-sm text-gray-600">
+              {identifier.includes("@")
+                ? `A password reset link has been sent to ${maskEmail(identifier)}.`
+                : `If an account exists to ${maskUsername(identifier)}, a reset link has been sent Mail Address`}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="text-center">
+            <p className="text-sm text-gray-500">
+              If you don’t see the email, check your spam folder or try
+              resending the link.
+            </p>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
