8e664e01a8942b10cdfdea4d1b03c92f6da8ef48
rjkush17 <rjkush17rishabh@gmail.com>
2025-11-04 22:42:10 +0530

    update sendmail function not it return true/fasle in complete

--- lib/nodemailer/sendMail.ts
+++ lib/nodemailer/sendMail.ts
@@ -8,21 +8,25 @@ export type mailDetailsType = {
   text?: string;
 };
 
-const SendMail = (mailDetails: mailDetailsType) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    secure: true,
-    port: 465,
-    auth: {
-      user: process.env.EMAIL_ID,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const SendMail = async (mailDetails: mailDetailsType): Promise<boolean> => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      port: 465,
+      auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  transporter
-    .sendMail(mailDetails)
-    .then((res) => console.log("email send successfully ", res))
-    .catch((e) => console.log("email send failed", e));
+    const info = await transporter.sendMail(mailDetails);
+    console.log("Email sent successfully:", info.response);
+    return true;
+  } catch (error) {
+    console.log("Email send failed:", error);
+    return false;
+  }
 };
 
 export default SendMail;
