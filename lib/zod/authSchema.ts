3e1bf0e9dd625c3756d30c59b648223f3db0924d
rjkush17 <rjkush17rishabh@gmail.com>
2025-11-05 09:30:07 +0530

    add new resetpassword schema

--- lib/zod/authSchema.ts
+++ lib/zod/authSchema.ts
@@ -56,3 +56,20 @@ export const forgotPasswordSchema = z.object({
 });
 
 export type forgotPasswordType = z.infer<typeof forgotPasswordSchema>;
+
+export const resetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(6, "Password must contain at least 6 characters")
+      .max(20, "Password must not exceed 20 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number"),
+
+    confirmPassword: z.string().min(6, "Confirm your password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+export type resetPasswordType = z.infer<typeof resetPasswordSchema>;
