7fde9e35f1cf7d3e63e3514bbb248061799fa14f
rjkush17 <rjkush17rishabh@gmail.com>
2026-01-28 22:14:28 +0530

    add new redirection to onboarding

--- proxy.ts
+++ proxy.ts
@@ -2,41 +2,45 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 
 export async function proxy(request: NextRequest) {
-    const session = await auth();
-    const { pathname } = request.nextUrl;
+  const session = await auth();
+  const { pathname } = request.nextUrl;
 
-    const publicPaths = ["/auth"];
-    const privatePaths = ["/onboarding", "/dashboard", "/profile"]; // example
+  const publicPaths = ["/auth"];
+  const privatePaths = ["/onboarding", "/dashboard", "/profile"]; // example
 
-    // --- #1 Public Routes: redirect logged-in users away ---
-    if (publicPaths.some((prefix) => pathname.startsWith(prefix))) {
-        if (session) return NextResponse.redirect(new URL("/", request.url));
+  // --- #1 Public Routes: redirect logged-in users away ---
+  if (publicPaths.some((prefix) => pathname.startsWith(prefix))) {
+    if (session) return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // --- #2 Private Routes: block unauthenticated users ---
+  if (privatePaths.some((prefix) => pathname.startsWith(prefix))) {
+    if (!session) return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // --- #3 Force onboarding if not completed ---
+  const role = session?.user.role;
+  if (
+    session &&
+    !session.user.onboardingVerified &&
+    !pathname.startsWith("/onboarding")
+  ) {
+    if (role === "individual") {
+      return NextResponse.redirect(new URL(`/onboarding/profile`, request.url));
+    } else if (role === "organization") {
+      return NextResponse.redirect(
+        new URL(`/onboarding/organization`, request.url),
+      );
+    } else {
+      return NextResponse.redirect(
+        new URL("/onboarding/profiletype", request.url),
+      );
     }
+  }
 
-    // --- #2 Private Routes: block unauthenticated users ---
-    if (privatePaths.some((prefix) => pathname.startsWith(prefix))) {
-        if (!session) return NextResponse.redirect(new URL("/", request.url));
-    }
-
-    // --- #3 Force onboarding if not completed ---
-    const role = session?.user.role;
-    if (
-        session &&
-        !session.user.onboardingVerified &&
-        !pathname.startsWith("/onboarding")
-    ) {
-        if (role) {
-            return NextResponse.redirect(new URL(`/onboarding/profile`, request.url));
-        } else {
-            return NextResponse.redirect(
-                new URL("/onboarding/profiletype", request.url),
-            );
-        }
-    }
-
-    return NextResponse.next();
+  return NextResponse.next();
 }
-
+//FIX: There is not backend API protection need to fixed if after onboarding creation
 export const config = {
-    matcher: ["/((?!_next|api|static|favicon.ico).*)"],
+  matcher: ["/((?!_next|static|favicon.ico).*)"],
 };
