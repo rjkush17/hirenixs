fd30f6e65ef7010171ae435c68f34ccf0395f706
rjkush17 <rjkush17rishabh@gmail.com>
2026-02-02 22:02:00 +0530

    api routes create for the submit onboaridng & indivudal data

---
+++ app/api/onboarding/individual/route.ts
@@ -0,0 +1,93 @@
+import connectDB from "@/database/Database";
+import { NextRequest, NextResponse } from "next/server";
+import { OnboardingSchema } from "@/lib/zod/individualProfile";
+import z from "zod";
+import { User, IUser } from "@/models/user";
+import { IUserProfile, UserProfile } from "@/models/userProfile";
+import { auth } from "@/auth";
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+    try {
+        await connectDB();
+        const { email, data } = await req.json();
+
+        if (!email)
+            return NextResponse.json({ error: "User ID not found" }, { status: 401 });
+
+        const session = await auth();
+        const userEmail = session?.user?.email;
+
+        if (!session && !userEmail) {
+            return NextResponse.json(
+                { error: "Unauthorized required" },
+                { status: 401 },
+            );
+        }
+
+        if (email !== userEmail) {
+            return NextResponse.json(
+                {
+                    error: "session ID not same as provided by request",
+                },
+                { status: 200 },
+            );
+        }
+
+        let cleanData;
+        try {
+            cleanData = OnboardingSchema.parse(data);
+        } catch (error) {
+            console.log("error in zod validation ", error);
+            if (error instanceof z.ZodError) {
+                console.log("zod issues: ", error.issues);
+                console.log("Field errors:", error.flatten().fieldErrors);
+                console.log("error instanceof zod ", error);
+            }
+            return NextResponse.json(
+                { error: "error while parsing data to zod" },
+                { status: 400 },
+            );
+        }
+
+        const user: IUser | null = await User.findOne({ email });
+        if (!user)
+            return NextResponse.json(
+                { error: "user and its profile not found " },
+                { status: 400 },
+            );
+
+        if (user.role !== "individual") {
+            return NextResponse.json(
+                { error: "user role type no found or incorrect" },
+                { status: 400 },
+            );
+        }
+        let userProfile: IUserProfile | null = await UserProfile.findOne({
+            userID: user._id,
+        });
+
+        if (!userProfile) {
+            userProfile = new UserProfile({
+                userID: user._id,
+                ...cleanData,
+            });
+        } else {
+            Object.assign(userProfile, cleanData);
+        }
+
+        await userProfile.save();
+        user.onboardingVerified = true;
+        await user.save();
+
+        return NextResponse.json(
+            { message: "User Profile Saved" },
+            { status: 200 },
+        );
+    } catch (error) {
+        console.log("error in the oboarding individual", error);
+        return NextResponse.json(
+            { error: "internal server Error" },
+            { status: 400 },
+        );
+    }
+};
