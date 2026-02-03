70aa46d3a871a873455d15787a7ba0ad08eced06
rjkush17 <rjkush17rishabh@gmail.com>
2026-02-03 09:26:27 +0530

    create a hook for check stepp and redirect on refresh

---
+++ hooks/useOnboardingRedirect.ts
@@ -0,0 +1,18 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAppSelector } from "@/hooks/useRedux";
+
+export function useOnboardingRedirect(requiredStep: number) {
+    const currentStep = useAppSelector(
+        (state) => state.onboardingStepChecker.step,
+    );
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (currentStep !== requiredStep) {
+            router.replace("/onboarding/individual/profile");
+        }
+    }, []);
+}
