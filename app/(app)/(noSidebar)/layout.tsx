5440ff4189ab7a53b65d2617e3f54e159f4c0fa1
rjkush17 <rjkush17rishabh@gmail.com>
2026-03-22 01:55:25 +0530

    make route group for sidebar and non sidebar pages

---
+++ app/(app)/(noSidebar)/layout.tsx
@@ -0,0 +1,60 @@
+import type { Metadata } from "next";
+import { Nunito_Sans } from "next/font/google";
+import "@/css/app/global.css";
+import Providers from "@/utils/Wrapper";
+import Sidebar from "@/components/sidebar/Sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import Footer from "@/components/Footer/Footer";
+
+const nunito_sans = Nunito_Sans({
+    weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-nunito_sans",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "hirenixs",
+    description:
+        "Personal portfolio showcasing modern web development projects, skills in MERN stack, Next.js, TypeScript, Rust, and Linux-based workflows. Focused on clean UI, performance, and scalable frontend architecture.",
+    keywords: [
+        "web developer portfolio",
+        "frontend developer",
+        "Next.js developer",
+        "React developer",
+        "MERN stack",
+        "JavaScript",
+        "TypeScript",
+        "Rust programming",
+        "Linux developer",
+        "Arch Linux",
+        "UI development",
+        "performance optimized websites",
+        "static site generation",
+        "SSG Next.js",
+    ],
+    icons: {
+        icon: [{ url: "/favicons/favicon.ico" }],
+        apple: [{ url: "/favicons/apple-touch-icon.png" }],
+    },
+
+    manifest: "/site.webmanifest",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={`${nunito_sans.className} antialiased`}>
+                <Providers>
+                    <div className="flex flex-col w-full">
+                        <main className="w-full">{children}</main>
+                        <Footer />
+                    </div>
+                </Providers>
+            </body>
+        </html>
+    );
+}
